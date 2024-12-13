41fd4d6794e5eb82223b720d7ca2042f796cbdc9
Yasvanth-2005 <yasvanthhanumantu1@gmail.com>
2024-12-13 17:53:47 +0530

    complaint roles

--- HackathonBackend/controllers/complaints.controllers.js
+++ HackathonBackend/controllers/complaints.controllers.js
@@ -101,117 +101,121 @@ export const updateComplaint = async (req, res) => {
 };
 
 export const getAllComplaints = async (req, res) => {
-  if (role !== "HOD" || role !== "DSW" || role !== "AO" || role !== "Warden") {
+  const role = req.role;
+
+  if (role === "HOD" || role === "DSW" || role === "AO" || role === "Warden") {
+    try {
+      const complaints = await Complaints.find();
+
+      if (!complaints) {
+        return res.status(404).json({ message: "Complaints Not Found" });
+      }
+
+      return res.status(200).json({ complaints });
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).json({ messgae: "Internal Server Error" });
+    }
+  } else {
     return res
       .status(404)
       .json({ message: "You are not allowed to see this data" });
-  }
-
-  try {
-    const complaints = await Complaints.find();
-
-    if (!complaints) {
-      return res.status(404).json({ message: "Complaints Not Found" });
-    }
-
-    return res.status(200).json({ complaints });
-  } catch (error) {
-    console.log(error.message);
-    return res.status(500).json({ messgae: "Internal Server Error" });
   }
 };
 
 export const getAOComplaints = async (req, res) => {
-  if (role !== "HOD" || role !== "DSW" || role !== "AO") {
+  const role = req.role;
+  if (role === "HOD" || role === "DSW" || role === "AO") {
+    try {
+      const threeDaysAgo = new Date();
+      threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
+
+      const complaints = await Complaints.find({
+        $or: [{ isCritical: true }, { createdAt: { $gte: threeDaysAgo } }],
+      });
+
+      if (complaints.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "No critical or recent complaints found" });
+      }
+
+      return res.status(200).json({
+        message: "Critical or recent complaints fetched successfully",
+        complaints,
+      });
+    } catch (error) {
+      console.error("Error fetching complaints:", error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  } else {
     return res
       .status(404)
       .json({ message: "You are not allowed to see this data" });
-  }
-
-  try {
-    const threeDaysAgo = new Date();
-    threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
-
-    const complaints = await Complaints.find({
-      $or: [{ isCritical: true }, { createdAt: { $gte: threeDaysAgo } }],
-    });
-
-    if (complaints.length === 0) {
-      return res
-        .status(404)
-        .json({ message: "No critical or recent complaints found" });
-    }
-
-    return res.status(200).json({
-      message: "Critical or recent complaints fetched successfully",
-      complaints,
-    });
-  } catch (error) {
-    console.error("Error fetching complaints:", error);
-    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
 export const getDSWComplaints = async (req, res) => {
-  if (role !== "HOD" || role !== "DSW") {
+  const role = req.role;
+  if (role === "HOD" || role === "DSW") {
+    try {
+      const threeDaysAgo = new Date();
+      threeDaysAgo.setDate(threeDaysAgo.getDate() - 7);
+
+      const complaints = await Complaints.find({
+        $or: [{ isCritical: true }, { createdAt: { $gte: threeDaysAgo } }],
+      });
+
+      if (complaints.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "No critical or recent complaints found" });
+      }
+
+      return res.status(200).json({
+        message: "Critical or recent complaints fetched successfully",
+        complaints,
+      });
+    } catch (error) {
+      console.error("Error fetching complaints:", error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  } else {
     return res
       .status(404)
       .json({ message: "You are not allowed to see this data" });
-  }
-
-  try {
-    const threeDaysAgo = new Date();
-    threeDaysAgo.setDate(threeDaysAgo.getDate() - 7);
-
-    const complaints = await Complaints.find({
-      $or: [{ isCritical: true }, { createdAt: { $gte: threeDaysAgo } }],
-    });
-
-    if (complaints.length === 0) {
-      return res
-        .status(404)
-        .json({ message: "No critical or recent complaints found" });
-    }
-
-    return res.status(200).json({
-      message: "Critical or recent complaints fetched successfully",
-      complaints,
-    });
-  } catch (error) {
-    console.error("Error fetching complaints:", error);
-    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
 export const getHODComplaints = async (req, res) => {
   const role = req.role;
-  try {
-    if (role !== "HOD") {
-      return res
-        .status(404)
-        .json({ message: "You are not allowed to see this data" });
+  if (role === "HOD") {
+    try {
+      const threeDaysAgo = new Date();
+      threeDaysAgo.setDate(threeDaysAgo.getDate() - 10);
+
+      const complaints = await Complaints.find({
+        $or: [{ isCritical: true }, { createdAt: { $gte: threeDaysAgo } }],
+      });
+
+      if (complaints.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "No critical or recent complaints found" });
+      }
+
+      return res.status(200).json({
+        message: "Critical or recent complaints fetched successfully",
+        complaints,
+      });
+    } catch (error) {
+      console.error("Error fetching complaints:", error);
+      return res.status(500).json({ message: "Internal Server Error" });
     }
-
-    const threeDaysAgo = new Date();
-    threeDaysAgo.setDate(threeDaysAgo.getDate() - 10);
-
-    const complaints = await Complaints.find({
-      $or: [{ isCritical: true }, { createdAt: { $gte: threeDaysAgo } }],
-    });
-
-    if (complaints.length === 0) {
-      return res
-        .status(404)
-        .json({ message: "No critical or recent complaints found" });
-    }
-
-    return res.status(200).json({
-      message: "Critical or recent complaints fetched successfully",
-      complaints,
-    });
-  } catch (error) {
-    console.error("Error fetching complaints:", error);
-    return res.status(500).json({ message: "Internal Server Error" });
+  } else {
+    return res
+      .status(404)
+      .json({ message: "You are not allowed to see this data" });
   }
 };
 

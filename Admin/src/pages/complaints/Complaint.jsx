7c5afb97086b61d19923c64db683ddb4656e8a46
unknown <yasvanthhanumantu1@gmail.com>
2025-02-25 13:09:23 +0530

    changes

--- Admin/src/pages/complaints/Complaint.jsx
+++ Admin/src/pages/complaints/Complaint.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useParams, useNavigate } from "react-router-dom";
 import Layout from "../../components/layout/Layout";
 import toast from "react-hot-toast";
+import { MdClose } from "react-icons/md";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -10,7 +11,7 @@ const statusStyles = {
   Pending: "bg-yellow-100 text-yellow-800",
   Rejected: "bg-red-100 text-red-800",
   Solved: "bg-green-100 text-green-800",
-  New: "bg-green-100 text-green-800",
+  New: "bg-blue-100 text-blue-800",
 };
 
 const ComplaintDashboard = () => {
@@ -18,9 +19,12 @@ const ComplaintDashboard = () => {
   const navigate = useNavigate();
   const [complaints, setComplaints] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [updateLoading, setUpdateLoading] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedComplaint, setSelectedComplaint] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [adminDescription, setAdminDescription] = useState("");
+  const [newStatus, setNewStatus] = useState("");
   const user = useSelector((state) => state.auth.user);
   const token = localStorage.getItem("authToken");
 
@@ -48,29 +52,26 @@ const ComplaintDashboard = () => {
   }, []);
 
   useEffect(() => {
-    // Sync input field with URL query
     if (paramQuery !== searchQuery) {
       setSearchQuery(paramQuery || "");
     }
-  }, [paramQuery]);
+  }, [paramQuery, searchQuery]);
 
   const handleSearchChange = (e) => {
     const value = e.target.value;
     setSearchQuery(value);
-    navigate(`/complaint/${value}`); // Update URL on search
+    navigate(`/complaint/${value}`);
   };
 
   const filteredComplaints = complaints.filter(
     (complaint) =>
-      complaint.typeOfComplaint
-        ?.toLowerCase()
-        .includes(searchQuery.toLowerCase()) ||
+      complaint.category?.toLowerCase().includes(searchQuery.toLowerCase()) ||
       complaint.status?.toLowerCase().includes(searchQuery.toLowerCase()) ||
       new Date(complaint.createdAt)
         .toLocaleString()
         ?.toLowerCase()
         .includes(searchQuery.toLowerCase()) ||
-      complaint.category?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      complaint.workplace?.toLowerCase().includes(searchQuery.toLowerCase()) ||
       complaint.userId?.username
         ?.toLowerCase()
         .includes(searchQuery.toLowerCase()) ||
@@ -79,34 +80,57 @@ const ComplaintDashboard = () => {
         .includes(searchQuery.toLowerCase())
   );
 
-  const handleStatusChange = async (complaintId, newStatus) => {
+  const handleStatusChange = async () => {
+    if (!newStatus || !adminDescription) {
+      toast.error("Please provide both status and description");
+      return;
+    }
+
     try {
-      const response = await fetch(`${apiUrl}/complaints/${complaintId}`, {
-        method: "PUT",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ status: newStatus }),
-      });
+      setUpdateLoading(true);
+      const response = await fetch(
+        `${apiUrl}/complaints/${selectedComplaint._id}`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            status: newStatus,
+            admin_description: adminDescription,
+          }),
+        }
+      );
       if (!response.ok) {
         throw new Error("Failed to update complaint status");
       }
       setComplaints(
         complaints.map((c) =>
-          c._id === complaintId ? { ...c, status: newStatus } : c
+          c._id === selectedComplaint._id
+            ? { ...c, status: newStatus, admin_description: adminDescription }
+            : c
         )
       );
-      setSelectedComplaint({ ...selectedComplaint, status: newStatus });
-      toast.success("status updated successfully");
+      setSelectedComplaint({
+        ...selectedComplaint,
+        status: newStatus,
+        admin_description: adminDescription,
+      });
+      toast.success("Status updated successfully");
+      setIsModalOpen(false);
+      setAdminDescription("");
+      setNewStatus("");
     } catch (error) {
       toast.error(error.message);
+    } finally {
+      setUpdateLoading(false);
     }
   };
 
   return (
     <Layout>
-      <div className="container mx-auto ">
+      <div className="container mx-auto">
         <div className="bg-white shadow-md rounded-lg p-2">
           <h1 className="text-3xl font-bold text-blue-800 mb-6">
             Complaints Dashboard
@@ -219,114 +243,159 @@ const ComplaintDashboard = () => {
         {isModalOpen && selectedComplaint && (
           <div
             onClick={() => setIsModalOpen(false)}
-            className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center p-4"
+            className="fixed inset-0 z-30 bg-black bg-opacity-50 flex justify-center items-center"
           >
             <div
               onClick={(e) => e.stopPropagation()}
-              className="bg-white max-h-[70vh] overflow-y-auto rounded-lg p-6 w-full max-w-2xl"
+              className="bg-white max-h-[90vh] overflow-y-auto rounded-lg w-full max-w-2xl"
             >
-              <h2 className="text-2xl font-bold text-blue-800 mb-4">
-                Complaint Details
-              </h2>
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
-                <div>
-                  <p className="font-semibold">
-                    Category:{" "}
-                    <span className="font-normal">
-                      {selectedComplaint.category}
-                    </span>
-                  </p>
-                  <p className="font-semibold">
-                    Workplace:{" "}
-                    <span className="font-normal">
-                      {selectedComplaint.workplace}
-                    </span>
-                  </p>
-                  <p className="font-semibold">
-                    Section:{" "}
-                    <span className="font-normal">
-                      {selectedComplaint.section}
-                    </span>
-                  </p>
+              <div className="w-full bg-white shadow-md mb-4 px-3 py-2 sticky top-0 flex items-center justify-between">
+                <div className="text-2xl font-bold text-blue-800 ">
+                  Complaint Details
                 </div>
-                <div className="flex flex-col gap-2">
-                  <p className="font-semibold">
-                    Status:
-                    <span
-                      className={`ml-2 px-2 py-1 text-xs font-semibold rounded-full ${
-                        statusStyles[selectedComplaint.status]
-                      }`}
-                    >
-                      {selectedComplaint.status}
-                    </span>
-                  </p>
-                  <p className="font-semibold">
-                    Critical:
-                    <span
-                      className={`ml-2 px-2 py-1 text-xs font-semibold rounded-full ${
-                        selectedComplaint.isCritical
-                          ? "bg-red-100 text-red-800"
-                          : "bg-gray-100 text-gray-800"
-                      }`}
-                    >
-                      {selectedComplaint.isCritical ? "Yes" : "No"}
-                    </span>
-                  </p>
-                  {selectedComplaint.dateAndTime && (
+                <MdClose
+                  className="text-2xl cursor-pointer"
+                  onClick={() => setIsModalOpen(false)}
+                />
+              </div>
+              <div className=" p-6">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
+                  <div className="col-span-2 max-md:col-span-1">
                     <p className="font-semibold">
-                      Date & Time:{" "}
+                      Acknowledgement Id:{" "}
                       <span className="font-normal">
-                        {new Date(
-                          selectedComplaint.dateAndTime
-                        ).toLocaleString()}
+                        {selectedComplaint.acknowledgementId}
                       </span>
                     </p>
-                  )}
+                    <p className="font-semibold">
+                      Category:{" "}
+                      <span className="font-normal">
+                        {selectedComplaint.category}
+                      </span>
+                    </p>
+                    <p className="font-semibold">
+                      Workplace:{" "}
+                      <span className="font-normal">
+                        {selectedComplaint.workplace}
+                      </span>
+                    </p>
+                    <p className="font-semibold">
+                      Section:{" "}
+                      <span className="font-normal">
+                        {selectedComplaint.section}
+                      </span>
+                    </p>
+                  </div>
+                  <div className="flex flex-col col-span-1 gap-2">
+                    <p className="font-semibold">
+                      Status:
+                      <span
+                        className={`ml-2 px-2 py-1 text-xs font-semibold rounded-full ${
+                          statusStyles[selectedComplaint.status]
+                        }`}
+                      >
+                        {selectedComplaint.status}
+                      </span>
+                    </p>
+                    <p className="font-semibold">
+                      Critical:
+                      <span
+                        className={`ml-2 px-2 py-1 text-xs font-semibold rounded-full ${
+                          selectedComplaint.isCritical
+                            ? "bg-red-100 text-red-800"
+                            : "bg-gray-100 text-gray-800"
+                        }`}
+                      >
+                        {selectedComplaint.isCritical ? "Yes" : "No"}
+                      </span>
+                    </p>
+                    {selectedComplaint.dateAndTime && (
+                      <p className="font-semibold">
+                        Date & Time:{" "}
+                        <span className="font-normal">
+                          {new Date(
+                            selectedComplaint.dateAndTime
+                          ).toLocaleString()}
+                        </span>
+                      </p>
+                    )}
+                  </div>
                 </div>
-              </div>
-              <div className="mb-4">
-                <p className="font-semibold">Description:</p>
-                <p className="mt-1 text-gray-600">
-                  {selectedComplaint.description}
-                </p>
-              </div>
-              {selectedComplaint.photo && (
                 <div className="mb-4">
-                  <p className="font-semibold">Photo Evidence:</p>
-                  <img
-                    src={selectedComplaint.photo}
-                    alt="Complaint Evidence"
-                    className="mt-2 max-w-full rounded-md w-24 h-24"
-                  />
+                  <p className="font-semibold">Description:</p>
+                  <p className="mt-1 text-gray-600">
+                    {selectedComplaint.description}
+                  </p>
                 </div>
-              )}
-              <div className="mb-4">
-                <p className="font-semibold mb-2">Change Status:</p>
-                <div className="flex space-x-2">
-                  {["New", "Pending", "Solved", "Rejected"].map((status) => (
+                {selectedComplaint.photo && (
+                  <div className="mb-4">
+                    <p className="font-semibold">Photo Evidence:</p>
+                    <img
+                      src={selectedComplaint.photo || "/placeholder.svg"}
+                      alt="Complaint Evidence"
+                      className="mt-2 max-w-full rounded-md w-24 h-24"
+                    />
+                  </div>
+                )}
+                {selectedComplaint.admin_description &&
+                  selectedComplaint.admin_role && (
+                    <div className="mb-4">
+                      <p className="font-semibold">Admin Description:</p>
+                      <p className="mt-1 text-gray-600">
+                        {selectedComplaint.admin_role} :{" "}
+                        {selectedComplaint.admin_description}
+                      </p>
+                    </div>
+                  )}
+                <div className="mb-4">
+                  <p className="font-semibold mb-2">Change Status:</p>
+                  <div className="flex flex-col space-y-2">
+                    <select
+                      value={newStatus}
+                      onChange={(e) => setNewStatus(e.target.value)}
+                      className="w-full p-2 border rounded-md"
+                    >
+                      <option value="">Select new status</option>
+                      {["Pending", "Solved", "Rejected"].map((status) => (
+                        <option key={status} value={status}>
+                          {status}
+                        </option>
+                      ))}
+                    </select>
+                    <textarea
+                      value={adminDescription}
+                      onChange={(e) => setAdminDescription(e.target.value)}
+                      placeholder="Provide a description for the status change"
+                      className="w-full p-2 border rounded-md"
+                      rows="3"
+                    ></textarea>
                     <button
-                      key={status}
-                      onClick={() =>
-                        handleStatusChange(selectedComplaint._id, status)
+                      onClick={handleStatusChange}
+                      disabled={
+                        newStatus === "" ||
+                        adminDescription === "" ||
+                        updateLoading
                       }
                       className={`px-4 py-2 rounded-md transition duration-300 ${
-                        selectedComplaint.status === status
-                          ? "bg-blue-600 text-white"
-                          : "bg-gray-200 text-gray-800 hover:bg-gray-300"
+                        newStatus === "" ||
+                        adminDescription === "" ||
+                        updateLoading
+                          ? "bg-gray-400 text-gray-200 cursor-not-allowed"
+                          : "bg-blue-600 text-white hover:bg-blue-700"
                       }`}
                     >
-                      {status}
+                      {updateLoading ? "Updating..." : "Update Status"}
                     </button>
-                  ))}
+                  </div>
                 </div>
-              </div>
-              <div className="flex justify-end mt-6">
-                <button
-                  onClick={() => setIsModalOpen(false)}
-                  className="px-4 py-2 bg-gray-300 text-black rounded-md hover:bg-gray-400 transition duration-300"
-                >
-                  Close
-                </button>
+                <div className="flex justify-end mt-6">
+                  <button
+                    onClick={() => setIsModalOpen(false)}
+                    className="px-4 py-1.5 bg-red-600 text-white font-semibold hover:bg-red-700  rounded-md hover:bg-gray-400 transition duration-300"
+                  >
+                    Close
+                  </button>
+                </div>
               </div>
             </div>
           </div>
